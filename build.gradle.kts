728c646727fca46fb27ffb594b26b9e58d79bed5
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-05-31 17:00:00 +0900

    :tada:

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktlint)
+}
+
+group = "me.bossm0n5t3r"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri(libs.versions.koog.maven.url.get())
+    }
+}
+
+dependencies {
+    implementation(libs.koog.agents)
+    testImplementation(libs.kotlin.test.junit5)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(
+        libs.versions.jdk.version
+            .get()
+            .toInt(),
+    )
+}
+
+ktlint {
+    version.set(
+        libs.versions.ktlint.version
+            .get(),
+    )
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package me.bossm0n5t3r
+
+fun main() {
+    println("Hello, Kotlin!")
+}
